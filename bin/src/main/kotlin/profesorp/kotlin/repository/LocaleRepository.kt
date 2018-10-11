2e521965a880a7b9e56aae4191c97bc568857df7
chuchip <chuchip@gmail.com>
2018-10-11 07:02:57 +0200

    Revert "Jugando con hateoas..."
    This reverts commit 3f021c669e444c13f2170573c72162e2745bd5c9.

--- bin/src/main/kotlin/profesorp/kotlin/repository/LocaleRepository.kt
+++
@@ -1,9 +0,0 @@
-package profesorp.kotlin.repository
-
-import org.springframework.data.repository.CrudRepository;
-import profesorp.kotlin.entity.Locales
-import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface LocaleRepository : JpaRepository<Locales,String> {
-}
